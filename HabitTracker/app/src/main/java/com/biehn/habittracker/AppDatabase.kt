7a706764eb12f4f65f8fbb533c28fa9dde6b9171
SooBiehn Hah <biehn.hah@gmail.com>
2026-08-05 09:10:33 +0900

    refactor: fix file casing.

---
+++ HabitTracker/app/src/main/java/com/biehn/habittracker/AppDatabase.kt
@@ -0,0 +1,11 @@
+package com.biehn.habittracker
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+@Database(entities = [Habit::class], version = 1, exportSchema = false)
+@TypeConverters(Converters::class)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun habitDao(): HabitDao
+}
